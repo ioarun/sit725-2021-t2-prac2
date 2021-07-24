39d66ed843b2d08692d108b47d982ef4bf95b4c2
IPTLP0032 <arun_kumar271@hotmail.com>
2021-07-24 19:20:31 +0530

    Add POST method

--- server.js
+++ server.js
@@ -4,8 +4,25 @@ var express = require("express");
 // Create app object
 var app = express()
 
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Declare port
 var port = process.env.port || 3000;
+
+
+/**
+ * Following are a few HTTPS methods.
+ * GET - fetches data from server
+ * POST - posts data to server
+ */
+
+// File upload service - POST
+app.post("/upload", (req, res) => {
+    console.log(req.body);
+    res.json({statusCode: 200, data: req.body.fileName, message: "Upload successful!"});
+});
 
 // Start the server
 app.listen(port, ()=>{
